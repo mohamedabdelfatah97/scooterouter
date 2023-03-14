765718d1a33eded0a23ba330faa1dfefb24ca5b2
Mohamed Abdelfatah <mohamedmahmoudelsayed97@gmail.com>
2023-03-14 16:48:46 +0200

    Graph: add nearestNode() to snap GPS coordinates to road network

---
+++ include/core/Graph.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Node.h"
+#include "Edge.h"
+#include <unordered_map>
+#include <vector>
+#include <optional>
+
+namespace sr {
+
+class Graph {
+public:
+    void addNode(const Node& node);
+    void addEdge(const Edge& edge);
+
+    const Node&              getNode(NodeId id) const;
+    const std::vector<Edge>& neighbors(NodeId id) const;
+
+    bool hasNode(NodeId id) const;
+    size_t nodeCount() const;
+    size_t edgeCount() const;
+
+    // Used by CoordinateProjector to compute bounding box
+    LatLon minBounds() const;
+    LatLon maxBounds() const;
+
+    // Used by DStarLite to update edge cost at runtime
+    void updateEdgeCost(NodeId from, NodeId to, Cost new_cost);
+
+    const std::unordered_map<NodeId, Node>& allNodes() const;
+    NodeId nearestNode(const LatLon& geo) const;
+
+private:
+    std::unordered_map<NodeId, Node>              nodes_;
+    std::unordered_map<NodeId, std::vector<Edge>> adjacency_;
+    size_t edge_count_ = 0;
+};
+
+} // namespace sr
