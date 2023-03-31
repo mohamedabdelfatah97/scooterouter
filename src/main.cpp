c5a7afdf87e75da350e286580d266d04057f37b1
Mohamed Abdelfatah <mohamedmahmoudelsayed97@gmail.com>
2023-03-31 21:06:15 +0200

    main.cpp: fix van spawn and scooter generation within map margins, add fallback routing

--- src/main.cpp
+++ src/main.cpp
@@ -18,22 +18,21 @@ int main(int argc, char** argv) {
     std::string map_path   = "data/maps/hamburg.osm";
     std::string fleet_path = "";
     std::string heuristic  = "euclidean";
-    int width    = 1400;
-    int height   = 800;
+    int width      = 1400;
+    int height     = 800;
     int n_scooters = 15;
-    int seed     = -1; // -1 = random seed each run
+    int seed       = -1;
 
-    app.add_option("--map",       map_path,    "Path to .osm map file");
-    app.add_option("--fleet",     fleet_path,  "Path to fleet CSV (optional)");
-    app.add_option("--heuristic", heuristic,   "Heuristic: euclidean|manhattan|octile");
-    app.add_option("--width",     width,       "Window width");
-    app.add_option("--height",    height,      "Window height");
-    app.add_option("--scooters",  n_scooters,  "Number of scooters to generate");
-    app.add_option("--seed",      seed,        "Random seed (-1 = random each run)");
+    app.add_option("--map",       map_path,   "Path to .osm map file");
+    app.add_option("--fleet",     fleet_path, "Path to fleet CSV (optional)");
+    app.add_option("--heuristic", heuristic,  "Heuristic: euclidean|manhattan|octile");
+    app.add_option("--width",     width,      "Window width");
+    app.add_option("--height",    height,     "Window height");
+    app.add_option("--scooters",  n_scooters, "Number of scooters to generate");
+    app.add_option("--seed",      seed,       "Random seed (-1 = random each run)");
 
     CLI11_PARSE(app, argc, argv);
 
-    // determine seed
     unsigned int actual_seed = (seed < 0)
         ? std::random_device{}()
         : static_cast<unsigned int>(seed);
@@ -68,21 +67,32 @@ int main(int argc, char** argv) {
     fmt::print("      {} scooters ({} collectible, {} critical)\n\n",
                fleet.all().size(), fleet.collectible().size(), fleet.critical().size());
 
-    // Random van spawn
+    // random van spawn within map margins
     std::mt19937 rng(actual_seed + 1);
-    auto nodes = graph.allNodes();
-    std::vector<sr::NodeId> node_ids;
-    for (const auto& [id, _] : nodes) node_ids.push_back(id);
-    std::uniform_int_distribution<size_t> dist(0, node_ids.size() - 1);
-    sr::NodeId van_node = node_ids[dist(rng)];
+    sr::LatLon min_b = graph.minBounds();
+    sr::LatLon max_b = graph.maxBounds();
+    double lat_margin = (max_b.lat - min_b.lat) * 0.1;
+    double lon_margin = (max_b.lon - min_b.lon) * 0.1;
+
+    std::vector<sr::NodeId> valid_nodes;
+    for (const auto& [id, node] : graph.allNodes()) {
+        if (!graph.neighbors(id).empty() &&
+            node.geo.lat > min_b.lat + lat_margin &&
+            node.geo.lat < max_b.lat - lat_margin &&
+            node.geo.lon > min_b.lon + lon_margin &&
+            node.geo.lon < max_b.lon - lon_margin)
+            valid_nodes.push_back(id);
+    }
+    std::uniform_int_distribution<size_t> dist(0, valid_nodes.size() - 1);
+    sr::NodeId van_node = valid_nodes[dist(rng)];
     sr::LatLon van_pos  = graph.getNode(van_node).geo;
     fmt::print("      van spawn: ({:.4f}, {:.4f})\n", van_pos.lat, van_pos.lon);
 
-    // Fixed warehouse location — right of Hamburg center
-    sr::LatLon warehouse_pos = { 53.5580, 10.0150 };
+    // fixed warehouse — right of Hamburg center
+    sr::LatLon warehouse_pos  = { 53.5580, 10.0150 };
     sr::NodeId warehouse_node = graph.nearestNode(warehouse_pos);
 
-    // Build optimized route: van → scooters → warehouse
+    // build optimized route: van → scooters → warehouse
     fmt::print("[test] Building optimized route...\n");
     sr::PriorityScorer scorer;
     auto ranked   = scorer.ranked(fleet.collectible(), van_pos);
@@ -97,10 +107,24 @@ int main(int argc, char** argv) {
     for (const auto& s : optimized) {
         sr::NodeId target = graph.nearestNode(s.geo);
         auto result = astar.plan(graph, current, target);
+
+        // if no path found, try neighbors of target node
+        if (result.path.empty()) {
+            for (const auto& edge : graph.neighbors(target)) {
+                result = astar.plan(graph, current, edge.to);
+                if (!result.path.empty()) {
+                    target = edge.to;
+                    break;
+                }
+            }
+        }
+
         if (!result.path.empty()) {
             full_path.insert(full_path.end(), result.path.begin(), result.path.end());
             total_cost += result.cost;
             current = target;
+        } else {
+            fmt::print("      WARNING: no path to scooter {} — skipping\n", s.id);
         }
     }
 
@@ -122,19 +146,6 @@ int main(int argc, char** argv) {
 
     sr::MissionController mission(fleet, optimizer, graph);
     renderer.run(mission, graph, fleet, full_path, van_pos, warehouse_pos);
-
-    for (const auto& s : optimized) {
-        sr::NodeId target = graph.nearestNode(s.geo);
-        auto result = astar.plan(graph, current, target);
-        if (!result.path.empty()) {
-            full_path.insert(full_path.end(), result.path.begin(), result.path.end());
-            total_cost += result.cost;
-            current = target;
-        } else {
-            fmt::print("      WARNING: no path to scooter {} at ({:.4f}, {:.4f}) — skipping\n",
-                       s.id, s.geo.lat, s.geo.lon);
-        }
-    }
 
     return 0;
 }
