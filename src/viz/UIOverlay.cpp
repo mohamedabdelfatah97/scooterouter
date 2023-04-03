e2d26d92cdcae7474b6294c13502a705dd11b960
Mohamed Abdelfatah <mohamedmahmoudelsayed97@gmail.com>
2023-04-03 14:48:00 +0200

    UIOverlay: add destructor and stub implementations to fix linker error

--- src/viz/UIOverlay.cpp
+++ src/viz/UIOverlay.cpp
@@ -2,6 +2,16 @@
 
 namespace sr {
 
-// TODO: implement UIOverlay
+UIOverlay::~UIOverlay() {
+    if (font_)       TTF_CloseFont(font_);
+    if (font_small_) TTF_CloseFont(font_small_);
+}
+
+bool UIOverlay::init(SDL_Renderer* r) { return true; }
+
+void UIOverlay::draw(SDL_Renderer* r,
+                     const MissionController& mission,
+                     const FleetManager& fleet,
+                     int panel_x, int panel_w, int screen_h) {}
 
 } // namespace sr
