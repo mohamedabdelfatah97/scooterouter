adf2a0a80366ca1ea7cded23ed02f3db6c65ec7b
Mohamed Abdelfatah <mohamedmahmoudelsayed97@gmail.com>
2023-05-11 08:44:22 +0200

    PathLayer: add PathColor struct and per-algorithm color constants

--- include/viz/PathLayer.h
+++ include/viz/PathLayer.h
@@ -6,13 +6,24 @@
 
 namespace sr {
 
+struct PathColor {
+    Uint8 r, g, b;
+};
+
+// predefined algorithm colors
+static constexpr PathColor PATH_ASTAR    = { 0,   100, 255 };
+static constexpr PathColor PATH_DIJKSTRA = { 0,   220, 100 };
+static constexpr PathColor PATH_BFS      = { 255, 80,  180 };
+static constexpr PathColor PATH_DSTAR    = { 255, 220, 0   };
+
 class PathLayer {
 public:
     void draw(SDL_Renderer* r,
               const std::vector<NodeId>& path,
               const Graph& graph,
               const CoordinateProjector& proj,
-              size_t visited_up_to);   // nodes before this index drawn gray
+              size_t visited_up_to,
+              PathColor color = PATH_ASTAR);
 };
 
 } // namespace sr
